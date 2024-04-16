9a2a0b5ccb08d9d9e98bd135900cf7b717b6b328
Roberto Viviani home new <roberto.viviani@uibk.ac.at>
2024-04-16 12:50:50 +0200

    embeddings, openAI large language model added, files for small renamed

--- importOpenAiEmbeddings.py
+++ importOpenAiEmbeddings.py
@@ -3,14 +3,14 @@
 """
 import pandas as pd
 
-def openAiEmbeddings(items):
+def openAiEmbeddings(items, model_name="text-embedding-3-small"):
     """Retrieves embeddings from openAI"""
 
     #retrieve embeddings from openAI
     from openai import OpenAI
     client = OpenAI()
 
-    def get_embedding(text, model="text-embedding-3-small"):
+    def get_embedding(text, model=model_name):
         text = text.replace("\n", " ")
         return client.embeddings.create(input = [text], model=model).data[0].embedding
 
@@ -21,8 +21,14 @@ def openAiEmbeddings(items):
 
 
 data = pd.read_csv('scales.csv')
-data['embedding'] = openAiEmbeddings(data['item'])
-data.to_csv('scales_openAI.csv', index=False)
+
+# retrieve embeddings from openAI small model
+data['embedding'] = openAiEmbeddings(data['item'], "text-embedding-3-small")
+data.to_csv('scales_openAI_small.csv', index=False)
+
+# retrieve embeddings from openAI large model
+data['embedding'] = openAiEmbeddings(data['item'], "text-embedding-3-large")
+data.to_csv('scales_openAI_large.csv', index=False)
 
 
 
