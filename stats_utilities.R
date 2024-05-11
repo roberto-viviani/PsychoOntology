9343de9942c9747825c40b57ae1558be1d86e05f
Roberto Viviani home2 <roberto.viviani@uibk.ac.at>
2024-05-11 15:28:38 +0200

    stats utilities

--- stats_utilities.R
+++ stats_utilities.R
@@ -1,6 +1,25 @@
 # Utilities to estimate stats
 
 # utility to estimate pearson correlations repeatedly
+pearscorr_ext <- function(data, yvar, xvars) {
+  stats <- data.frame()
+  for (v in xvars) {
+    cs <- cor.test(pull(data,yvar), pull(data,v), 
+                   method = "pearson", alternative = "two.sided",
+                   na.action = "na.omit")
+    stats[v,"r"] <- cs$estimate
+    stats[v,"lci"] <- cs$conf.int[1]
+    stats[v,"uci"] <- cs$conf.int[2]
+    stats[v,"t"] <- cs$statistic
+    stats[v,"p"] <- cs$p.value
+    stats[v, "pcorr"] <- ifelse(cs$p.value < 0.001 / length(xvars) / 5, "***", 
+                                ifelse(cs$p.value < 0.01 / length(xvars) / 5, "**", 
+                                       ifelse(cs$p.value < 0.05 / length(xvars) / 5, 
+                                              "*", "-")))
+  }
+  stats
+}
+
 pearscorr <- function(data, yvar, xvars) {
   stats <- data.frame()
   for (v in xvars) {
