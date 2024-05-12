e24e65838d04977c29e881d447b95c3da0e3c926
Roberto Viviani home2 <roberto.viviani@uibk.ac.at>
2024-05-12 16:21:24 +0200

    mistral english embeddings added (with no real improvement)

--- importMistralEmbeddings.py
+++ importMistralEmbeddings.py
@@ -9,7 +9,7 @@ api_key = os.environ["MISTRAL_API_KEY"]
 embed_model = "mistral-embed"
 
 # check if the 'scales_mistral_de.csv' file exists
-if os.path.exists('embeddings_mistral_de.csv'):
+if os.path.exists('embeddings_mistral_en.csv'):
     # raise error if the file exists
     raise FileExistsError('The file embeddings_mistral_de.csv ' + 
         'already exists. Please remove it before running this script again.')
@@ -18,9 +18,9 @@ data = pd.read_csv('scales.csv')
 
 # if the scales items are language specific, rename the column to 
 # 'item' and drop the other language column
-if 'item_de' in data.columns:
-    data = data.rename(columns={'item_de': 'item'})
-    data = data.drop(columns=['item_en'])
+if 'item_en' in data.columns:
+    data = data.rename(columns={'item_en': 'item'})
+    data = data.drop(columns=['item_de'])
 
 # create a MistralClient object
 client = MistralClient(api_key=api_key)
@@ -33,5 +33,5 @@ response = client.embeddings(
 
 # collecct embeddings from response and save them to the 'scales_mistral_de.csv' file
 data['embedding'] = [x.embedding for x in response.data]
-data.to_csv('embeddings_mistral_de.csv', index=False)
+data.to_csv('embeddings_mistral_en.csv', index=False)
 
