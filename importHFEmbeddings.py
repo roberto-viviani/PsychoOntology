b01686ac513cc4c25485ad6ef0348ff6bfb459bf
Roberto Viviani home2 <roberto.viviani@uibk.ac.at>
2024-05-13 14:21:52 +0200

    roberta multilingual

--- importHFEmbeddings.py
+++ importHFEmbeddings.py
@@ -48,25 +48,52 @@ if 'item_de' in data.columns:
 
 # embeddings = [text_embedder.run(item)['embedding'] for item in data.item]
 
-# # save embeddings to the 'scales_roberta_de.csv' file
+# # save embeddings to file
 # data['embedding'] = embeddings
 # data.to_csv('embeddings_mpnetbasev2_de.csv', index=False)
 
+# # check if the embedding file exists
+# if os.path.exists('embeddings_bert_msmarco_de.csv'):
+#     # raise error if the file exists
+#     raise FileExistsError('The file embeddings_mpnetbasev2_de.csv ' + 
+#         'already exists. Please remove it before running this script again.')
+
+# # a model specifically for German language
+# text_embedder = HuggingFaceTEITextEmbedder(
+#     model="PM-AI/bi-encoder_msmarco_bert-base_german",
+#     token=Secret.from_token(os.environ['HF_API_KEY'])
+# )
+
+# embeddings = [text_embedder.run(item)['embedding'] for item in data.item]
+
+# # save embeddings to file
+# data['embedding'] = embeddings
+# data.to_csv('embeddings_bert_msmarco_de.csv', index=False)
+
+# multilingual roberta model. This does not work under windows, use google colab instead
 # check if the embedding file exists
-if os.path.exists('embeddings_bert_msmarco_de.csv'):
+if os.path.exists('embeddings_roberta_multilingual_de.csv'):
     # raise error if the file exists
     raise FileExistsError('The file embeddings_mpnetbasev2_de.csv ' + 
         'already exists. Please remove it before running this script again.')
 
-# a model specifically for German language
-text_embedder = HuggingFaceTEITextEmbedder(
-    model="PM-AI/bi-encoder_msmarco_bert-base_german", 
-    token=Secret.from_token(os.environ['HF_API_KEY'])
-)
+# multilingual roberta model
+from sentence_transformers import SentenceTransformer
 
-embeddings = [text_embedder.run(item)['embedding'] for item in data.item]
+# Download from the 🤗 Hub
+model = SentenceTransformer("tomaarsen/xlm-roberta-base-multilingual-en-ar-fr-de-es-tr-it")
+# Run inference
+sentences = data.item.tolist()
+embeddings = model.encode(sentences)
 
-# save embeddings to the 'scales_roberta_de.csv' file
-data['embedding'] = embeddings
-data.to_csv('embeddings_bert_msmarco_de.csv', index=False)
+# save embeddings to file
+data['embedding'] = embeddings.tolist()
+data.to_csv('embeddings_roberta_multilingual_de.csv', index=False)
 
+# multilingual from China
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer('intfloat/multilingual-e5-large-instruct')
+
+embeddings = model.encode(data.input.tolist(), convert_to_tensor=True, normalize_embeddings=True)
+data['embedding'] = embeddings.tolist()
+data.to_csv('embeddings_multilingual_e5_large_instruct_de.csv', index=False)
