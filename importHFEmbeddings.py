2ce4eace3d842ba372a238a4a4e31ae9ead2a32d
Roberto Viviani home2 <roberto.viviani@uibk.ac.at>
2024-05-13 09:14:05 +0200

    a German-specific multilingual bert model

--- importHFEmbeddings.py
+++ importHFEmbeddings.py
@@ -15,34 +15,52 @@ if 'item_de' in data.columns:
     data = data.rename(columns={'item_de': 'item'})
     data = data.drop(columns=['item_en'])
 
-# check if the 'embeddings_roberta_de.csv' file exists
-if os.path.exists('embeddings_roberta_de.csv'):
-    # raise error if the file exists
-    raise FileExistsError('The file embeddings_roberta_de.csv ' + 
-        'already exists. Please remove it before running this script again.')
+# # check if the 'embeddings_roberta_de.csv' file exists
+# if os.path.exists('embeddings_roberta_de.csv'):
+#     # raise error if the file exists
+#     raise FileExistsError('The file embeddings_roberta_de.csv ' + 
+#         'already exists. Please remove it before running this script again.')
 
-# retrieve embeddings from roberta from hugging face
-# https://huggingface.co/sentence-transformers/all-roberta-large-v1
-text_embedder = HuggingFaceTEITextEmbedder(
-    model="sentence-transformers/all-roberta-large-v1", 
-    token=Secret.from_token(os.environ['HF_API_KEY'])
-)
+# # retrieve embeddings from roberta from hugging face
+# # https://huggingface.co/sentence-transformers/all-roberta-large-v1
+# text_embedder = HuggingFaceTEITextEmbedder(
+#     model="sentence-transformers/all-roberta-large-v1", 
+#     token=Secret.from_token(os.environ['HF_API_KEY'])
+# )
 
-embeddings = [text_embedder.run(item)['embedding'] for item in data.item]
+# embeddings = [text_embedder.run(item)['embedding'] for item in data.item]
 
-# save embeddings to the 'scales_roberta_de.csv' file
-data['embedding'] = embeddings
-data.to_csv('embeddings_roberta_de.csv', index=False)
+# # save embeddings to the 'scales_roberta_de.csv' file
+# data['embedding'] = embeddings
+# data.to_csv('embeddings_roberta_de.csv', index=False)
+
+# # check if the embedding file exists
+# if os.path.exists('embeddings_mpnetbasev2_de.csv'):
+#     # raise error if the file exists
+#     raise FileExistsError('The file embeddings_mpnetbasev2_de.csv ' + 
+#         'already exists. Please remove it before running this script again.')
+
+# # a model recommended for its performance (https://www.sbert.net/docs/pretrained_models.html)
+# text_embedder = HuggingFaceTEITextEmbedder(
+#     model="sentence-transformers/paraphrase-multilingual-mpnet-base-v2", 
+#     token=Secret.from_token(os.environ['HF_API_KEY'])
+# )
+
+# embeddings = [text_embedder.run(item)['embedding'] for item in data.item]
+
+# # save embeddings to the 'scales_roberta_de.csv' file
+# data['embedding'] = embeddings
+# data.to_csv('embeddings_mpnetbasev2_de.csv', index=False)
 
 # check if the embedding file exists
-if os.path.exists('embeddings_mpnetbasev2_de.csv'):
+if os.path.exists('embeddings_bert_msmarco_de.csv'):
     # raise error if the file exists
     raise FileExistsError('The file embeddings_mpnetbasev2_de.csv ' + 
         'already exists. Please remove it before running this script again.')
 
-# a model recommended for its performance (https://www.sbert.net/docs/pretrained_models.html)
+# a model specifically for German language
 text_embedder = HuggingFaceTEITextEmbedder(
-    model="sentence-transformers/paraphrase-multilingual-mpnet-base-v2", 
+    model="PM-AI/bi-encoder_msmarco_bert-base_german", 
     token=Secret.from_token(os.environ['HF_API_KEY'])
 )
 
@@ -50,6 +68,5 @@ embeddings = [text_embedder.run(item)['embedding'] for item in data.item]
 
 # save embeddings to the 'scales_roberta_de.csv' file
 data['embedding'] = embeddings
-data.to_csv('embeddings_mpnetbasev2_de.csv', index=False)
-
+data.to_csv('embeddings_bert_msmarco_de.csv', index=False)
 
